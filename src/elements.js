2dcbd20394d5d80c096482750c26aa615218845c
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-03 08:41:57 +0100

    Wrapped each returned element in an Element instance

--- src/elements.js
+++ src/elements.js
@@ -1,5 +1,17 @@
-const elements = (matcher) => {
-  return matcher.resolve(true);
+const { Element } = require("./element");
+
+const elements = async (matcher) => {
+  const response = await matcher.resolve(true);
+
+  return response.value.map((element) => {
+    const value = {
+      status: response.status,
+      value: element,
+      sessionId: response.sessionId
+    };
+
+    return new Element(Promise.resolve(value));
+  });
 };
 
 module.exports = elements;
