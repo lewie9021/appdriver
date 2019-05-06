a92a4954b52cd3baa9c7131b091665a0fa924569
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-06 10:00:30 +0100

    Added tests around a few common gestures.

--- __tests__/Gesture.spec.js
+++ __tests__/Gesture.spec.js
@@ -147,7 +147,7 @@ describe("release", () => {
         pointerType: "touch"
       },
       actions: [
-        {"type": "pointerUp", "button": 0}
+        {type: "pointerUp", button: 0}
       ]
     });
   });
@@ -158,5 +158,95 @@ describe("release", () => {
     const result = gesture.release();
 
     expect(result).toBeInstanceOf(Gesture);
+  });
+});
+
+describe("Common Gestures", () => {
+  it("correctly defines a tap gesture", () => {
+    const tap = new Gesture()
+      .press({x: 0, y: 0})
+      .wait({duration: 100})
+      .release();
+
+    expect(tap._getActions()).toEqual({
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: "viewport", x: 0, y: 0},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 100},
+        {type: "pointerUp", button: 0}
+      ]
+    })
+  });
+
+  it("correctly defines a long press gesture", () => {
+    const longPress = new Gesture()
+      .press({x: 0, y: 0})
+      .wait({duration: 1000})
+      .release();
+
+    expect(longPress._getActions()).toEqual({
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: "viewport", x: 0, y: 0},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 1000},
+        {type: "pointerUp", button: 0}
+      ]
+    });
+  });
+
+  it("correctly defines a swipe left gesture", () => {
+    const longPress = new Gesture()
+      .press({x: 150, y: 200})
+      .wait({duration: 250})
+      .moveTo({x: -100, y: 0, relative: true, duration: 50})
+      .release();
+
+    expect(longPress._getActions()).toEqual({
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: "viewport", x: 150, y: 200},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 250},
+        {type: "pointerMove", duration: 50, origin: "pointer", x: -100, y: 0},
+        {type: "pointerUp", button: 0}
+      ]
+    });
+  });
+
+  it("correctly defines a swipe right gesture", () => {
+    const longPress = new Gesture()
+      .press({x: 150, y: 200})
+      .wait({duration: 250})
+      .moveTo({x: 100, y: 0, relative: true, duration: 50})
+      .release();
+
+    expect(longPress._getActions()).toEqual({
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: "viewport", x: 150, y: 200},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 250},
+        {type: "pointerMove", duration: 50, origin: "pointer", x: 100, y: 0},
+        {type: "pointerUp", button: 0}
+      ]
+    });
   });
 });
