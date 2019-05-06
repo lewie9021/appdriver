e2240f969f1d3a72834d56f42cb414648a7d2142
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-06 17:32:27 +0100

    Added tests around gestures.swipeRight.

--- __tests__/gestures.spec.js
+++ __tests__/gestures.spec.js
@@ -35,3 +35,30 @@ describe("swipeLeft", () => {
     });
   });
 });
+
+describe("swipeRight", () => {
+  it("returns a 'Gesture'", () => {
+    const result = gestures.swipeRight({x: 150, y: 200, distance: 100});
+
+    expect(result).toBeInstanceOf(Gesture);
+  });
+
+  it("correctly defines a swipe right gesture", () => {
+    const result = gestures.swipeRight({x: 150, y: 200, distance: 100});
+
+    expect(result.resolve()).toEqual({
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: "viewport", x: 150, y: 200},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 250},
+        {type: "pointerMove", duration: 50, origin: "pointer", x: 100, y: 0},
+        {type: "pointerUp", button: 0}
+      ]
+    });
+  });
+});
