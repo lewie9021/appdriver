81ba6c350d65a1ae8e50ba9956d310a3d9fe72e3
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-11 11:18:58 +0100

    Fixed logs.

--- examples/native-demo-app/tests/carousel-screen.spec.js
+++ examples/native-demo-app/tests/carousel-screen.spec.js
@@ -7,10 +7,10 @@ describe("Carousel Screen", () => {
     console.log("[1] Waiting for screen to load...");
     await element(by.label(screenTestId)).waitToBeVisible();
 
-    console.log("[3] Swiping to next page...");
+    console.log("[2] Swiping to next page...");
     await device.swipeLeft({y: 300, percentage: 0.75});
 
-    console.log("[4] Swiping to next page...");
+    console.log("[3] Swiping to next page...");
     await device.swipeLeft({y: 300, percentage: 0.75});
   });
 });
