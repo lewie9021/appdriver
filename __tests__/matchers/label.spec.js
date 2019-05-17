e815be8a02f0ae2bdc8bb84142d6e8fbbd6b3773
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-17 21:31:00 +0100

    Refactored label tests.

--- __tests__/matchers/label.spec.js
+++ __tests__/matchers/label.spec.js
@@ -8,12 +8,6 @@ const { createElementsFixture } = require("../fixtures/fixtures");
 const mockCommand = require("../helpers/mockCommand");
 const mockSession = require("../helpers/mockSession");
 
-beforeEach(() => {
-  mockSession({
-    platformName: "iOS"
-  });
-});
-
 afterEach(() => {
   jest.resetAllMocks();
 });
@@ -24,78 +18,72 @@ it("returns an object containing a resolve method to execute the query", () => {
   expect(result).toHaveProperty("resolve");
 });
 
-it("supports fetching a single matching element", async () => {
-  const accessibilityLabel = "button";
-  mockCommand(commands.element.findElement, () => createElementFixture({elementId: "elementId"}));
+const testPlatform = () => {
+  it("supports fetching a single matching element", async () => {
+    const accessibilityLabel = "button";
+    mockCommand(commands.element.findElement, () => createElementFixture({elementId: "elementId"}));
 
-  const matcher = by.label(accessibilityLabel);
+    const matcher = by.label(accessibilityLabel);
 
-  await matcher.resolve();
+    await matcher.resolve();
 
-  expect(commands.element.findElement).toHaveBeenCalledTimes(1);
-  expect(commands.element.findElement.mock.calls[0][0]).toMatchSnapshot();
+    expect(commands.element.findElement).toHaveBeenCalledTimes(1);
+    expect(commands.element.findElement.mock.calls[0][0]).toMatchSnapshot();
+  });
+
+  it("supports fetching multiple matching elements", async () => {
+    const accessibilityLabel = "button";
+    mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
+
+    const matcher = by.label(accessibilityLabel);
+
+    await matcher.resolve(true);
+
+    expect(commands.element.findElements).toHaveBeenCalledTimes(1);
+    expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
+  });
+
+  it("supports fetching elements starting with a given accessibility label", async () => {
+    const accessibilityLabel = "list-item-*";
+    mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
+
+    const matcher = by.label(accessibilityLabel);
+
+    await matcher.resolve(true);
+
+    expect(commands.element.findElements).toHaveBeenCalledTimes(1);
+    expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
+  });
+
+  it("supports fetching elements containing the a accessibility label", async () => {
+    const accessibilityLabel = "*-item-*";
+    mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
+
+    const matcher = by.label(accessibilityLabel);
+
+    await matcher.resolve(true);
+
+    expect(commands.element.findElements).toHaveBeenCalledTimes(1);
+    expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
+  });
+};
+
+describe("iOS", () => {
+  beforeEach(() => {
+    mockSession({
+      platformName: "iOS"
+    });
+  });
+
+  testPlatform();
 });
 
-it("supports fetching multiple matching elements", async () => {
-  const accessibilityLabel = "button";
-  mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
+describe("Android", () => {
+  beforeEach(() => {
+    mockSession({
+      platformName: "Android"
+    });
+  });
 
-  const matcher = by.label(accessibilityLabel);
-
-  await matcher.resolve(true);
-
-  expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-  expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
-});
-
-it("supports fetching elements starting with a given accessibility label (iOS)", async () => {
-  const accessibilityLabel = "list-item-*";
-  mockSession({platformName: "iOS"});
-  mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
-
-  const matcher = by.label(accessibilityLabel);
-
-  await matcher.resolve(true);
-
-  expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-  expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
-});
-
-it("supports fetching elements starting with a given accessibility label (Android)", async () => {
-  const accessibilityLabel = "list-item-*";
-  mockSession({platformName: "Android"});
-  mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
-
-  const matcher = by.label(accessibilityLabel);
-
-  await matcher.resolve(true);
-
-  expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-  expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
-});
-
-it("supports fetching elements containing the a accessibility label (iOS)", async () => {
-  const accessibilityLabel = "*-item-*";
-  mockSession({platformName: "iOS"});
-  mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
-
-  const matcher = by.label(accessibilityLabel);
-
-  await matcher.resolve(true);
-
-  expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-  expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
-});
-
-it("supports fetching elements containing the a accessibility label (Android)", async () => {
-  const accessibilityLabel = "*-item-*";
-  mockSession({platformName: "Android"});
-  mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
-
-  const matcher = by.label(accessibilityLabel);
-
-  await matcher.resolve(true);
-
-  expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-  expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
+  testPlatform();
 });
