fcfa5736cba2ad10c61ac526a1d8da2749121102
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-26 16:33:23 +0100

    Rewrote tests for label matcher.

--- __tests__/matchers/label.spec.js
+++ __tests__/matchers/label.spec.js
@@ -1,14 +1,13 @@
-jest.mock("../../src/commands");
-jest.mock("../../src/session");
-const commands = require("../../src/commands");
+const appiumServer = require("../helpers/appiumServer");
+const fetch = require("node-fetch");
 
-const { by } = require("../../src/matchers");
-const { createElementFixture } = require("../fixtures/fixtures");
-const { createElementsFixture } = require("../fixtures/fixtures");
-const mockCommand = require("../helpers/mockCommand");
+jest.mock("../../src/session");
 const mockSession = require("../helpers/mockSession");
 
+const { by } = require("../../");
+
 afterEach(() => {
+  appiumServer.resetMocks();
   jest.resetAllMocks();
 });
 
@@ -21,69 +20,147 @@ it("returns an object containing a resolve method to execute the query", () => {
 const testPlatform = () => {
   it("supports fetching a single matching element", async () => {
     const accessibilityLabel = "button";
-    mockCommand(commands.element.findElement, () => createElementFixture({elementId: "elementId"}));
+    appiumServer.mockFindElement({elementId: "elementId"});
 
     const matcher = by.label(accessibilityLabel);
 
     await matcher.resolve();
 
-    expect(commands.element.findElement).toHaveBeenCalledTimes(1);
-    expect(commands.element.findElement.mock.calls[0][0]).toMatchSnapshot();
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        method: "POST",
+        body: JSON.stringify({
+          using: "accessibility id",
+          value: accessibilityLabel,
+        })
+      })
+    );
   });
 
   it("supports fetching multiple matching elements", async () => {
     const accessibilityLabel = "button";
-    mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
+    appiumServer.mockFindElements({elements: ["element-0", "element-1"]});
 
     const matcher = by.label(accessibilityLabel);
 
     await matcher.resolve(true);
 
-    expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-    expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
-  });
-
-  it("supports fetching elements starting with a given accessibility label", async () => {
-    const accessibilityLabel = "list-item-*";
-    mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
-
-    const matcher = by.label(accessibilityLabel);
-
-    await matcher.resolve(true);
-
-    expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-    expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
-  });
-
-  it("supports fetching elements containing the a accessibility label", async () => {
-    const accessibilityLabel = "*-item-*";
-    mockCommand(commands.element.findElements, () => createElementsFixture({elementIds: ["element-0", "element-1"]}));
-
-    const matcher = by.label(accessibilityLabel);
-
-    await matcher.resolve(true);
-
-    expect(commands.element.findElements).toHaveBeenCalledTimes(1);
-    expect(commands.element.findElements.mock.calls[0][0]).toMatchSnapshot();
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        method: "POST",
+        body: JSON.stringify({
+          using: "accessibility id",
+          value: accessibilityLabel,
+        })
+      })
+    );
   });
 };
 
 describe("iOS", () => {
   beforeEach(() => {
     mockSession({
+      sessionId: "sessionId",
       platformName: "iOS"
     });
   });
 
   testPlatform();
+
+  it("supports fetching elements starting with a given accessibility label", async () => {
+    appiumServer.mockFindElements({elements: ["element-0", "element-1"]});
+
+    const matcher = by.label("list-item-*");
+
+    await matcher.resolve(true);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        method: "POST",
+        body: JSON.stringify({
+          using: "-ios predicate string",
+          value: "name BEGINSWITH 'list-item-'",
+        })
+      })
+    );
+  });
+
+  it("supports fetching elements containing the a accessibility label", async () => {
+    const accessibilityLabel = "*-item-*";
+    appiumServer.mockFindElements({elements: ["element-0", "element-1"]});
+
+    const matcher = by.label(accessibilityLabel);
+
+    await matcher.resolve(true);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        method: "POST",
+        body: JSON.stringify({
+          using: "-ios predicate string",
+          value: "name CONTAINS '-item-'",
+        })
+      })
+    );
+  });
 });
 
 describe("Android", () => {
   beforeEach(() => {
     mockSession({
+      sessionId: "sessionId",
       platformName: "Android"
     });
   });
 
   testPlatform();
+
+  it("supports fetching elements starting with a given accessibility label", async () => {
+    appiumServer.mockFindElements({elements: ["element-0", "element-1"]});
+
+    const matcher = by.label("list-item-*");
+
+    await matcher.resolve(true);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        method: "POST",
+        body: JSON.stringify({
+          using: "-android uiautomator",
+          value: "new UiSelector().descriptionStartsWith(\"list-item-\")",
+        })
+      })
+    );
+  });
+
+  it("supports fetching elements containing the a accessibility label", async () => {
+    const accessibilityLabel = "*-item-*";
+    appiumServer.mockFindElements({elements: ["element-0", "element-1"]});
+
+    const matcher = by.label(accessibilityLabel);
+
+    await matcher.resolve(true);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        method: "POST",
+        body: JSON.stringify({
+          using: "-android uiautomator",
+          value: "new UiSelector().descriptionContains(\"-item-\")",
+        })
+      })
+    );
+  });
 });
