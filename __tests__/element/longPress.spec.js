7ac26ccc4448da7e1a436403afa822691716a7df
Lewis Barnes <lb.barnes@live.co.uk>
2019-10-23 08:59:45 +0100

    Added coverage around x and y parameters for .tap on Element. Simplified .longPress tests.

--- __tests__/element/longPress.spec.js
+++ __tests__/element/longPress.spec.js
@@ -51,70 +51,36 @@ it("executes a long press gesture", async () => {
   );
 });
 
-it("accepts an x parameter to offset from the left of the element", async () => {
+it("accepts x and y parameters to offset from the top left of the element", async () => {
   const elementId = "elementId";
-  const x = 100;
+  const x = 32;
+  const y = 64;
 
-  appiumServer.mockFindElement({elementId});
-  appiumServer.mockActions();
+  const findElementMock = appiumServer.mockFindElement({elementId});
+  const longPressElementMock = appiumServer.mockActions();
 
-  await element(by.label("button")).longPress({x});
+  await element(by.label("button")).longPress({ x, y });
 
-  expect(fetch).toHaveBeenCalledTimes(2);
-  expect(fetch).toHaveBeenLastCalledWith(
-    expect.any(String),
-    expect.objectContaining({
-      method: "POST",
-      body: JSON.stringify({
-        actions: [{
-          id: "finger1",
-          type: "pointer",
-          parameters: {
-            pointerType: "touch"
-          },
-          actions: [
-            {type: "pointerMove", duration: 0, origin: {element: elementId}, x, y: 0},
-            {type: "pointerDown", button: 0},
-            {type: "pause", duration: 750},
-            {type: "pointerUp", button: 0}
-          ]
-        }]
-      })
-    })
-  );
-});
+  const findElementMockCalls = appiumServer.getCalls(findElementMock);
+  const longPressElementMockCalls = appiumServer.getCalls(longPressElementMock);
 
-it("accepts an y parameter to offset from the top of the element", async () => {
-  const elementId = "elementId";
-  const y = 32;
-
-  appiumServer.mockFindElement({elementId});
-  appiumServer.mockActions();
-
-  await element(by.label("button")).longPress({y});
-
-  expect(fetch).toHaveBeenCalledTimes(2);
-  expect(fetch).toHaveBeenLastCalledWith(
-    expect.any(String),
-    expect.objectContaining({
-      method: "POST",
-      body: JSON.stringify({
-        actions: [{
-          id: "finger1",
-          type: "pointer",
-          parameters: {
-            pointerType: "touch"
-          },
-          actions: [
-            {type: "pointerMove", duration: 0, origin: {element: elementId}, x: 0, y},
-            {type: "pointerDown", button: 0},
-            {type: "pause", duration: 750},
-            {type: "pointerUp", button: 0}
-          ]
-        }]
-      })
-    })
-  );
+  expect(findElementMockCalls).toHaveLength(1);
+  expect(longPressElementMockCalls).toHaveLength(1);
+  expect(longPressElementMockCalls[0].options.body).toEqual({
+    actions: [{
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: {element: elementId}, x, y},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 750},
+        {type: "pointerUp", button: 0}
+      ]
+    }]
+  });
 });
 
 it("accepts a duration parameter to redefine how long to perform the press action", async () => {
--- __tests__/element/tap.spec.js
+++ __tests__/element/tap.spec.js
@@ -18,6 +18,68 @@ it("returns an instance of Element to enable function chaining", async () => {
   await expect($element.value).resolves.toEqual("elementId");
 });
 
+it("executes a tap gesture", async () => {
+  const elementId = "elementId";
+
+  const findElementMock = appiumServer.mockFindElement({elementId});
+  const tapElementMock = appiumServer.mockActions();
+
+  await element(by.label("button")).tap();
+
+  const findElementMockCalls = appiumServer.getCalls(findElementMock);
+  const tapElementMockCalls = appiumServer.getCalls(tapElementMock);
+
+  expect(findElementMockCalls).toHaveLength(1);
+  expect(tapElementMockCalls).toHaveLength(1);
+  expect(tapElementMockCalls[0].options.body).toEqual({
+    actions: [{
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: {element: elementId}, x: 0, y: 0},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 100},
+        {type: "pointerUp", button: 0}
+      ]
+    }]
+  });
+});
+
+it("accepts x and y parameters to offset from the top left of the element", async () => {
+  const elementId = "elementId";
+  const x = 100;
+  const y = 24;
+
+  const findElementMock = appiumServer.mockFindElement({elementId});
+  const tapElementMock = appiumServer.mockActions();
+
+  await element(by.label("button")).tap({ x, y });
+
+  const findElementMockCalls = appiumServer.getCalls(findElementMock);
+  const tapElementMockCalls = appiumServer.getCalls(tapElementMock);
+
+  expect(findElementMockCalls).toHaveLength(1);
+  expect(tapElementMockCalls).toHaveLength(1);
+  expect(tapElementMockCalls[0].options.body).toEqual({
+    actions: [{
+      id: "finger1",
+      type: "pointer",
+      parameters: {
+        pointerType: "touch"
+      },
+      actions: [
+        {type: "pointerMove", duration: 0, origin: {element: elementId}, x, y},
+        {type: "pointerDown", button: 0},
+        {type: "pause", duration: 100},
+        {type: "pointerUp", button: 0}
+      ]
+    }]
+  });
+});
+
 it("returns a new element to avoid unwanted mutation", async () => {
   appiumServer.mockFindElement({elementId: "elementId"});
   appiumServer.mockActions();
