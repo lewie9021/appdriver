5db95903a5fb46e43b9612f7cc2536c9b395c4fd
Lewis Barnes <lb.barnes@live.co.uk>
2019-11-03 10:17:35 +0000

    Implemented element and interaction commands.

--- src/appium/appium.js
+++ src/appium/appium.js
@@ -1,65 +1,232 @@
 const api = require("./api");
 const { getSession } = require("../session");
-const { ElementNotFoundError, ElementsNotFoundError } = require("../errors");
+const { platform } = require("../utils");
 
-// findElement = ({ using: String, value: String, elementId?: String }) => Promise<String>.
-const findElement = ({ using, value, elementId }) => {
+// ({ matcher: AppiumMatcher, element: AppiumElement }) => Promise<AppiumElement>.
+const findElement = ({ matcher, element }) => {
   const sessionId = getSession("sessionId");
 
-  if (elementId) {
-    return api.post(`/session/${sessionId}/element/${elementId}/element`, null, { using, value })
-      .then(({status, value}) => {
+  if (element) {
+    return api.post(`/session/${sessionId}/element/${element.ELEMENT}/element`, null, matcher)
+      .then(({ status, value }) => {
         if (status) {
-          throw new ElementNotFoundError("Failed to find element from element.");
+          throw new Error("GENERAL_ERROR");
         }
 
-        return value.ELEMENT;
+        return value;
       });
   }
 
-  return api.post(`/session/${sessionId}/element`, null, { using, value })
+  return api.post(`/session/${sessionId}/element`, null, matcher)
     .then(({ status, value }) => {
       if (status) {
-        throw new ElementNotFoundError("Failed to find element.");
-      }
-
-      return value.ELEMENT;
-    });
-};
-
-// findElements = ({ using: String, value: String, elementId?: String }) => Promise<Array<String>>.
-const findElements = ({ using, value, elementId }) => {
-  const sessionId = getSession("sessionId");
-
-  if (elementId) {
-    return api.post(`/session/${sessionId}/element/${elementId}/elements`, null, { using, value })
-      .then(({status, value}) => {
-        if (status) {
-          throw new ElementsNotFoundError("Failed to find elements from element.");
-        }
-
-        return value.map((element) => element.ELEMENT);
-      });
-  }
-
-  return api.post(`/session/${sessionId}/elements`, null, { using, value })
-    .then(({status, value}) => {
-      if (status) {
-        throw new ElementsNotFoundError("Failed to find elements.");
-      }
-
-      return value.map((element) => element.ELEMENT);
-    });
-};
-
-// getElementAttribute = ({ elementId: String, attribute: String }) => Promise<String>.
-const getElementAttribute = ({ elementId, attribute }) => {
-  return api.get(`/session/${getSession("sessionId")}/element/${elementId}/attribute/${attribute}`)
-    .then(({ status, value }) => {
-      if (status) {
-        throw new Error("Failed to get attribute of element.");
+        throw new Error("GENERAL_ERROR");
       }
 
       return value;
+    });
+};
+
+// ({ matcher: AppiumMatcher, element: AppiumElement }) => Promise<Array<AppiumElement>>.
+const findElements = ({ matcher, element }) => {
+  const sessionId = getSession("sessionId");
+
+  if (element) {
+    return api.post(`/session/${sessionId}/element/${element.ELEMENT}/elements`, null, matcher)
+      .then(({ status, value }) => {
+        if (status) {
+          throw new Error("GENERAL_ERROR");
+        }
+
+        return value;
+      });
+  }
+
+  return api.post(`/session/${sessionId}/elements`, null, matcher)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement, attribute: String }) => Promise<String>.
+const getElementAttribute = ({ element, attribute }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/attribute/${attribute}`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<Boolean>.
+// Note: doesn't work on iOS yet. See https://github.com/appium/appium/issues/13441.
+const getElementSelected = ({ element }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/selected`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<Boolean>.
+const getElementDisabled = ({ element }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/enabled`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return !value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<Boolean>.
+const getElementVisible = ({ element }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/displayed`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<{ width: Number, height: Number }>.
+const getElementSize = ({ element }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/size`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<String>.
+const getElementName = ({ element }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/name`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<String>.
+const getElementType = ({ element }) => {
+  const spec = {
+    ios: () => getElementName({ element }),
+    android: () => getElementAttribute({ element, attribute: "className"})
+  };
+
+  return platform.select(spec)
+    .catch((err) => {
+      console.log(err);
+      throw new Error("GENERAL_ERROR");
+    });
+};
+
+// ({ matcher: AppiumMatcher })
+const getElementExists = ({ matcher }) => {
+  return findElement({ matcher })
+    .then(() => true)
+    .catch(() => false);
+};
+
+// ({ element: AppiumElement }) => Promise<String>.
+const getElementText = ({ element }) => {
+  return api.get(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/text`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement }) => Promise<String>.
+const getElementValue = ({ element }) => {
+  const spec = {
+    ios: () => getElementAttribute({ element, attribute: "value" }),
+    android: () => getElementText({ element })
+  };
+
+  return platform.select(spec)
+    .catch((err) => {
+      console.log(err);
+      throw new Error("GENERAL_ERROR");
+    });
+};
+
+// ({ element: AppiumElement, relative: Boolean }) => Promise<{ x: Number, y: Number }>.
+const getElementLocation = ({ element, relative }) => {
+  const sessionId = getSession("sessionId");
+
+  if (relative) {
+    return api.get(`/session/${sessionId}/element/${element.ELEMENT}/location_in_view`)
+      .then(({ status, value }) => {
+        if (status) {
+          throw new Error("GENERAL_ERROR");
+        }
+
+        return value;
+      });
+  }
+
+  return api.get(`/session/${sessionId}/element/${element.ELEMENT}/location`)
+    .then(({ status, value }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+
+      return value;
+    });
+};
+
+// ({ element: AppiumElement, text: String }) => Promise.
+const sendElementText = ({ element, text }) => {
+  return api.post(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/value`, null, { value: text })
+    .then(({ status }) => {
+      if (status) {
+        if (status === 13 && getSession("platformName") === "iOS") {
+          throw new Error("HARDWARE_KEYBOARD_CONNECTED");
+        }
+
+        throw new Error("GENERAL_ERROR");
+      }
+    });
+};
+
+// ({ element: AppiumElement }) => Promise.
+const clearElementText = ({ element }) => {
+  return api.post(`/session/${getSession("sessionId")}/element/${element.ELEMENT}/clear`)
+    .then(({ status }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
+    });
+};
+
+// ({ actions: W3CActions }) => Promise.
+const performActions = ({ actions }) => {
+  return api.post(`/session/${getSession("sessionId")}/actions`, null, { actions })
+    .then(({ status }) => {
+      if (status) {
+        throw new Error("GENERAL_ERROR");
+      }
     });
 };
