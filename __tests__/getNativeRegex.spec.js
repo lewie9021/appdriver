1957d7388633d578bf2a86d9f7eb6ba9258c4633
Lewis Barnes <lb.barnes@live.co.uk>
2019-12-13 07:23:56 +0000

    Added helper to convert JS regex to iOS Predicate & UiSelector regex.

---
+++ __tests__/getNativeRegex.spec.js
@@ -0,0 +1,45 @@
+jest.mock("../src/worker/stores/sessionStore");
+
+const { sessionStore } = require("../src/worker/stores/sessionStore");
+const getNativeRegex = require("../src/worker/helpers/getNativeRegex");
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+});
+
+describe("iOS", () => {
+  beforeEach(() => jest.spyOn(sessionStore, "getCapabilities").mockReturnValue("iOS"));
+
+  it("supports regex without flags", () => {
+    expect(getNativeRegex(/hello-world/)).toEqual({
+      pattern: "hello-world",
+      modifiers: ""
+    });
+  });
+
+  it("returns '[c]' when the case-insensitive flag is used", () => {
+    expect(getNativeRegex(/HELLO-WORLD/i)).toEqual({
+      pattern: "HELLO-WORLD",
+      modifiers: "[c]"
+    });
+  });
+});
+
+describe("Android", () => {
+  beforeEach(() => jest.spyOn(sessionStore, "getCapabilities").mockReturnValue("Android"));
+
+  it("supports regex without flags", () => {
+    expect(getNativeRegex(/hello-world/)).toEqual({
+      pattern: "hello-world",
+      modifiers: ""
+    });
+  });
+
+  it("returns '(?i)' when the case-insensitive flag is used", () => {
+    expect(getNativeRegex(/HELLO-WORLD/i)).toEqual({
+      pattern: "HELLO-WORLD",
+      modifiers: "(?i)"
+    });
+  });
+});
---
+++ src/worker/helpers/getNativeRegex.js
@@ -0,0 +1,18 @@
+const { platform } = require("../../utils");
+
+// Converts JS regex to 'native' (iOS predicate & UiSelector) regex.
+// TODO: Add tests.
+const getNativeRegex = (regex) => {
+  const pattern = regex.toString();
+  const flags = regex.flags.split("");
+
+  return {
+    pattern: pattern.slice(1, pattern.lastIndexOf("/")),
+    modifiers: platform.select({
+      ios: () => flags.includes("i")  ? "[c]" : "",
+      android: () => flags.includes("i") ? "(?i)" : ""
+    })
+  }
+};
+
+module.exports = getNativeRegex;
