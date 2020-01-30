a7ebade169d03c3120653261bd4ef94ee4e3b249
Lewis Barnes <lb.barnes@live.co.uk>
2020-01-30 07:48:14 +0000

    Added tests around all 3 platforms.

--- __tests__/appium-service/getElementNameAttribute.spec.js
+++ __tests__/appium-service/getElementNameAttribute.spec.js
@@ -1,46 +1,81 @@
 jest.mock("../../src/worker/services/request");
 jest.mock("../../src/worker/stores/sessionStore");
 
-const requestHelpers = require("../../src/worker/services/request");
 const { sessionStore } = require("../../src/worker/stores/sessionStore");
 const { appiumService } = require("../../src/worker/services/appiumService");
+const requestHelpers = require("../../src/worker/services/request");
+const { setPlatform } = require("../helpers");
 const { createFindElementMock } = require("../appiumServiceMocks");
+const { NotSupportedError } = require("../../src/worker/errors");
 
 afterEach(() => {
   jest.resetAllMocks();
   jest.restoreAllMocks();
 });
 
-it("makes a GET request to the correct Appium endpoint", async () => {
-  const sessionId = "sessionId";
-  const ref = createFindElementMock();
-  const name = "XCUIElementTypeOther";
-  jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
-  jest.spyOn(requestHelpers, "request").mockResolvedValue(name);
+const runTests = () => {
+  it("makes a GET request to the correct Appium endpoint", async () => {
+    const sessionId = "sessionId";
+    const ref = createFindElementMock();
+    const name = "XCUIElementTypeOther";
+    jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
+    jest.spyOn(requestHelpers, "request").mockResolvedValue(name);
 
-  await expect(appiumService.getElementNameAttribute({ element: ref }))
-    .resolves.toEqual(name);
+    await expect(appiumService.getElementNameAttribute({ element: ref }))
+      .resolves.toEqual(name);
 
-  expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-  expect(requestHelpers.request).toHaveBeenCalledWith({
-    method: "GET",
-    path: `/session/${sessionId}/element/${ref.ELEMENT}/name`
+    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
+    expect(requestHelpers.request).toHaveBeenCalledWith({
+      method: "GET",
+      path: `/session/${sessionId}/element/${ref.ELEMENT}/name`
+    });
   });
+
+  it("optionally accepts a sessionId", async () => {
+    const sessionId = "newSessionId";
+    const ref = createFindElementMock();
+    const name = "XCUIElementTypeTextField";
+    jest.spyOn(sessionStore, "getSessionId").mockReturnValue("sessionId");
+    jest.spyOn(requestHelpers, "request").mockResolvedValue(name);
+
+    await expect(appiumService.getElementNameAttribute({ sessionId, element: ref }))
+      .resolves.toEqual(name);
+
+    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
+    expect(requestHelpers.request).toHaveBeenCalledWith({
+      method: "GET",
+      path: `/session/${sessionId}/element/${ref.ELEMENT}/name`
+    });
+  });
+};
+
+describe("iOS", () => {
+  beforeEach(() => setPlatform("iOS"));
+  runTests();
 });
 
-it("optionally accepts a sessionId", async () => {
-  const sessionId = "newSessionId";
-  const ref = createFindElementMock();
-  const name = "XCUIElementTypeTextField";
-  jest.spyOn(sessionStore, "getSessionId").mockReturnValue("sessionId");
-  jest.spyOn(requestHelpers, "request").mockResolvedValue(name);
+describe("Android", () => {
+  beforeEach(() => setPlatform("Android"));
+  runTests();
+});
 
-  await expect(appiumService.getElementNameAttribute({ sessionId, element: ref }))
-    .resolves.toEqual(name);
+describe("Web", () => {
+  beforeEach(() => setPlatform("Web"));
 
-  expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-  expect(requestHelpers.request).toHaveBeenCalledWith({
-    method: "GET",
-    path: `/session/${sessionId}/element/${ref.ELEMENT}/name`
+  it("throws a NotSupportedError", async () => {
+    const sessionId = "sessionId";
+    const ref = createFindElementMock();
+    jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
+    jest.spyOn(requestHelpers, "request").mockResolvedValue();
+    expect.assertions(3);
+
+    try {
+      await appiumService.getElementEnabledAttribute({ element: ref });
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotSupportedError);
+      expect(err).toHaveProperty("message", "Functionality not supported.");
+    }
+
+    expect(requestHelpers.request).not.toHaveBeenCalled();
   });
 });
--- __tests__/appium-service/getElementVisibleAttribute.spec.js
+++ __tests__/appium-service/getElementVisibleAttribute.spec.js
@@ -1,46 +1,81 @@
 jest.mock("../../src/worker/services/request");
 jest.mock("../../src/worker/stores/sessionStore");
 
-const requestHelpers = require("../../src/worker/services/request");
 const { sessionStore } = require("../../src/worker/stores/sessionStore");
 const { appiumService } = require("../../src/worker/services/appiumService");
+const requestHelpers = require("../../src/worker/services/request");
+const { setPlatform } = require("../helpers");
 const { createFindElementMock } = require("../appiumServiceMocks");
+const { NotSupportedError } = require("../../src/worker/errors");
 
 afterEach(() => {
   jest.resetAllMocks();
   jest.restoreAllMocks();
 });
 
-it("makes a GET request to the correct Appium endpoint", async () => {
-  const sessionId = "sessionId";
-  const ref = createFindElementMock();
-  const visible = true;
-  jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
-  jest.spyOn(requestHelpers, "request").mockResolvedValue(visible);
+const runTests = () => {
+  it("makes a GET request to the correct Appium endpoint", async () => {
+    const sessionId = "sessionId";
+    const ref = createFindElementMock();
+    const visible = true;
+    jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
+    jest.spyOn(requestHelpers, "request").mockResolvedValue(visible);
 
-  await expect(appiumService.getElementVisibleAttribute({ element: ref }))
-    .resolves.toEqual(visible);
+    await expect(appiumService.getElementVisibleAttribute({ element: ref }))
+      .resolves.toEqual(visible);
 
-  expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-  expect(requestHelpers.request).toHaveBeenCalledWith({
-    method: "GET",
-    path: `/session/${sessionId}/element/${ref.ELEMENT}/displayed`
+    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
+    expect(requestHelpers.request).toHaveBeenCalledWith({
+      method: "GET",
+      path: `/session/${sessionId}/element/${ref.ELEMENT}/displayed`
+    });
   });
+
+  it("optionally accepts a sessionId", async () => {
+    const sessionId = "newSessionId";
+    const ref = createFindElementMock();
+    const visible = false;
+    jest.spyOn(sessionStore, "getSessionId").mockReturnValue("sessionId");
+    jest.spyOn(requestHelpers, "request").mockResolvedValue(visible);
+
+    await expect(appiumService.getElementVisibleAttribute({ sessionId, element: ref }))
+      .resolves.toEqual(visible);
+
+    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
+    expect(requestHelpers.request).toHaveBeenCalledWith({
+      method: "GET",
+      path: `/session/${sessionId}/element/${ref.ELEMENT}/displayed`
+    });
+  });
+};
+
+describe("iOS", () => {
+  beforeEach(() => setPlatform("iOS"));
+  runTests();
 });
 
-it("optionally accepts a sessionId", async () => {
-  const sessionId = "newSessionId";
-  const ref = createFindElementMock();
-  const visible = false;
-  jest.spyOn(sessionStore, "getSessionId").mockReturnValue("sessionId");
-  jest.spyOn(requestHelpers, "request").mockResolvedValue(visible);
+describe("Android", () => {
+  beforeEach(() => setPlatform("Android"));
+  runTests();
+});
 
-  await expect(appiumService.getElementVisibleAttribute({ sessionId, element: ref }))
-    .resolves.toEqual(visible);
+describe("Web", () => {
+  beforeEach(() => setPlatform("Web"));
 
-  expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-  expect(requestHelpers.request).toHaveBeenCalledWith({
-    method: "GET",
-    path: `/session/${sessionId}/element/${ref.ELEMENT}/displayed`
+  it("throws a NotSupportedError", async () => {
+    const sessionId = "sessionId";
+    const ref = createFindElementMock();
+    jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
+    jest.spyOn(requestHelpers, "request").mockResolvedValue();
+    expect.assertions(3);
+
+    try {
+      await appiumService.getElementVisibleAttribute({ element: ref });
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotSupportedError);
+      expect(err).toHaveProperty("message", "Functionality not supported.");
+    }
+
+    expect(requestHelpers.request).not.toHaveBeenCalled();
   });
 });
