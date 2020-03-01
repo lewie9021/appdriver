26570de97ffc5edf7a85b6ccb41dbb595178721e
Lewis Barnes <lb.barnes@live.co.uk>
2020-03-01 08:45:01 +0000

    Added tests around isFocused.

---
+++ __tests__/element/isFocused.spec.js
@@ -0,0 +1,108 @@
+jest.mock("../../src/worker/services/appiumService");
+
+const { appiumService } = require("../../src/worker/services/appiumService");
+const { createFindElementMock } = require("../appiumServiceMocks");
+const { setPlatform } = require("../helpers");
+const { ElementNotFoundError, ElementActionError, AppiumError } = require("../../src/worker/errors");
+const { element, by } = require("../../");
+
+beforeEach(() => setPlatform("iOS"));
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+});
+
+it("returns the element's focused status", async () => {
+  const ref = createFindElementMock();
+  const focused = true;
+
+  jest.spyOn(appiumService, "findElement").mockResolvedValue(ref);
+  jest.spyOn(appiumService, "getElementFocusedAttribute").mockResolvedValue(focused);
+
+  const result = await element(by.label("text-input")).isFocused();
+
+  expect(appiumService.findElement).toHaveBeenCalledTimes(1);
+  expect(appiumService.getElementFocusedAttribute).toHaveBeenCalledTimes(1);
+  expect(appiumService.getElementFocusedAttribute).toHaveBeenCalledWith(expect.objectContaining({ element: ref }));
+  expect(result).toEqual(focused);
+});
+
+it("throws an ElementNotFoundError if the element isn't found", async () => {
+  const error = new AppiumError("Request error.", 7);
+
+  jest.spyOn(appiumService, "findElement").mockRejectedValue(error);
+  jest.spyOn(appiumService, "getElementFocusedAttribute").mockResolvedValue(false);
+  expect.assertions(4);
+
+  try {
+    await element(by.label("text-input")).isFocused();
+  } catch (err) {
+    expect(err).toBeInstanceOf(ElementNotFoundError);
+    expect(err).toHaveProperty("message", `Failed to find element by label matching "text-input".`);
+  }
+
+  expect(appiumService.findElement).toHaveBeenCalledTimes(1);
+  expect(appiumService.getElementFocusedAttribute).toHaveBeenCalledTimes(0);
+});
+
+it("throws an ElementActionError for Appium request errors", async () => {
+  const ref = createFindElementMock();
+  const error = new AppiumError("Request error.", 3);
+
+  jest.spyOn(appiumService, "findElement").mockResolvedValue(ref);
+  jest.spyOn(appiumService, "getElementFocusedAttribute").mockRejectedValue(error);
+  expect.assertions(4);
+
+  try {
+    await element(by.label("text-input")).isFocused();
+  } catch (err) {
+    expect(err).toBeInstanceOf(ElementActionError);
+    expect(err).toHaveProperty("message", "Failed to retrieve focused status of element.");
+  }
+
+  expect(appiumService.findElement).toHaveBeenCalledTimes(1);
+  expect(appiumService.getElementFocusedAttribute).toHaveBeenCalledTimes(1);
+});
+
+it("propagates errors from further up the chain", async () => {
+  const ref = createFindElementMock();
+  const tapError = new AppiumError("Request error.", 3);
+
+  jest.spyOn(appiumService, "findElement").mockResolvedValue(ref);
+  jest.spyOn(appiumService, "tapElement").mockRejectedValue(tapError);
+  jest.spyOn(appiumService, "getElementFocusedAttribute").mockResolvedValue(true);
+  expect.assertions(5);
+
+  try {
+    await element(by.label("text-input"))
+      .tap()
+      .isFocused();
+  } catch (err) {
+    expect(err).toBeInstanceOf(ElementActionError);
+    expect(err).toHaveProperty("message", "Failed to tap element.");
+  }
+
+  expect(appiumService.findElement).toHaveBeenCalledTimes(1);
+  expect(appiumService.tapElement).toHaveBeenCalledTimes(1);
+  expect(appiumService.getElementFocusedAttribute).toHaveBeenCalledTimes(0);
+});
+
+it("propagates other types of errors", async () => {
+  const ref = createFindElementMock();
+  const error = new Error("Something went wrong.");
+
+  jest.spyOn(appiumService, "findElement").mockResolvedValue(ref);
+  jest.spyOn(appiumService, "getElementFocusedAttribute").mockRejectedValue(error);
+  expect.assertions(4);
+
+  try {
+    await element(by.label("text-input")).isFocused();
+  } catch (err) {
+    expect(err).toBeInstanceOf(error.constructor);
+    expect(err).toHaveProperty("message", error.message);
+  }
+
+  expect(appiumService.findElement).toHaveBeenCalledTimes(1);
+  expect(appiumService.getElementFocusedAttribute).toHaveBeenCalledTimes(1);
+});
