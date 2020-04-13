c4888e3dbfdd05b15933627604c0fa34b42abc2b
Lewis Barnes <lb.barnes@live.co.uk>
2020-04-13 07:07:35 +0100

    Hardened tests.

--- examples/native-demo-app/tests/alert-screen.e2e.js
+++ examples/native-demo-app/tests/alert-screen.e2e.js
@@ -1,31 +1,35 @@
 const { by, alert, device, element, expect } = require("../../../main");
 
 describe("Alert Screen", () => {
-  let $screen;
-
   before(async () => {
     await element(by.label("menu-screen")).waitToBeVisible();
     await element(by.label("list-item-alert-screen")).tap();
-    await element(by.label("menu-screen")).waitToBeInvisible();
-    $screen = await element(by.label("alert-screen")).waitToBeVisible();
+    await element(by.label("alert-screen")).waitToBeVisible();
   });
 
   afterEach(() => alert.dismiss());
 
   it("supports retrieving alert text", async () => {
+    const $screen = await element(by.label("alert-screen"));
     await $screen.findElement(by.label("alert-button")).tap();
 
-    await expect(alert.getText()).toEqual([
+    await device.waitFor(() => expect(alert.isVisible()).toBeTruthy());
+
+    return expect(alert.getText()).toEqual([
       "Alert",
       "Hello World!"
     ].join("\n"));
   });
 
   it("supports accepting alerts", async () => {
+    const $screen = await element(by.label("alert-screen"));
     await $screen.findElement(by.label("confirm-alert-button")).tap();
+
+    await device.waitFor(() => expect(alert.isVisible()).toBeTruthy());
+
     await alert.accept();
 
-    await device.waitFor(() => {
+    return device.waitFor(() => {
       return expect(alert.getText()).toEqual([
         "Alert",
         "Accepted!"
@@ -34,10 +38,14 @@ describe("Alert Screen", () => {
   });
 
   it("supports cancelling alerts", async () => {
+    const $screen = await element(by.label("alert-screen"));
     await $screen.findElement(by.label("confirm-alert-button")).tap();
+
+    await device.waitFor(() => expect(alert.isVisible()).toBeTruthy());
+
     await alert.dismiss();
 
-    await device.waitFor(() => {
+    return device.waitFor(() => {
       return expect(alert.getText()).toEqual([
         "Alert",
         "Cancelled!"
