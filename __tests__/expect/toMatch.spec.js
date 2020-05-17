0a58a8094385ee5c5c14356be52332cdf810dab9
Lewis Barnes <lb.barnes@live.co.uk>
2020-05-17 08:18:53 +0100

    Throw TypeError instead of NotImplementedError.

--- __tests__/expect/toMatch.spec.js
+++ __tests__/expect/toMatch.spec.js
@@ -1,4 +1,3 @@
-const { NotImplementedError } = require("../../src/worker/errors");
 const { expect: assert } = require("../../main");
 
 it("doesn't throw if expectation is met", async () => {
@@ -32,20 +31,20 @@ it("throws if expectation is not met when used with .not", async () => {
 
 it("throws if value to perform assertion on isn't a string", async () => {
   await expect(assert(undefined).toMatch(/test/))
-    .rejects.toThrow(NotImplementedError);
+    .rejects.toThrow(TypeError);
 
   await expect(assert(null).toMatch(/test/))
-    .rejects.toThrow(NotImplementedError);
+    .rejects.toThrow(TypeError);
 
   await expect(assert(true).toMatch(/test/))
-    .rejects.toThrow(NotImplementedError);
+    .rejects.toThrow(TypeError);
 
   await expect(assert(50).toMatch(/test/))
-    .rejects.toThrow(NotImplementedError);
+    .rejects.toThrow(TypeError);
 
   await expect(assert([ 1, 2, 3 ]).toMatch(/test/))
-    .rejects.toThrow(NotImplementedError);
+    .rejects.toThrow(TypeError);
 
   await expect(assert({ a: 1, b: 2 }).toMatch(/test/))
-    .rejects.toThrow(NotImplementedError);
+    .rejects.toThrow(TypeError);
 });
