ef14137b8567d429914150e249a7f3ef2e9227a3
Lewis Barnes <lb.barnes@live.co.uk>
2020-05-25 12:13:54 +0100

    Initial attempt to support Web.

--- src/worker/services/appdriver/scroll.js
+++ src/worker/services/appdriver/scroll.js
@@ -1,14 +1,32 @@
 const { sessionStore } = require("../../stores/sessionStore");
 const { appiumService } = require("../appiumService");
+const { platform } = require("../../../utils");
+const { getRelativePoint } = require("../../helpers/coordinates");
 const gestures = require("../../gestures");
 
 // ({ sessionId: String?, x: Number, y: Number, direction: Number, distance: Number }) => Promise.
 const scroll = async ({ sessionId = sessionStore.getSessionId(), x, y, direction, distance }) => {
-  const gesture = gestures.scroll({ x, y, direction, distance });
+  return platform.select({
+    native: () => {
+      const gesture = gestures.scroll({ x, y, direction, distance });
 
-  return appiumService.performActions({
-    sessionId,
-    actions: gesture.resolve()
+      return appiumService.performActions({
+        sessionId,
+        actions: gesture.resolve()
+      });
+    },
+    web: () => {
+      const relativePoint = getRelativePoint({ x, y, direction, distance });
+
+      return appiumService.execute({
+        sessionId,
+        script: "window.scrollBy(arguments[0], arguments[1])",
+        args: [
+          relativePoint.x * -1,
+          relativePoint.y * -1
+        ]
+      });
+    }
   });
 };
 
