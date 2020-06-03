2b4051e92a293654797b352f626c0a19a127fd78
Lewis Barnes <lb.barnes@live.co.uk>
2020-06-03 08:06:07 +0100

    Switched to test at the Appium service layer.

--- __tests__/appdriver-service/scroll.spec.js
+++ __tests__/appdriver-service/scroll.spec.js
@@ -1,11 +1,10 @@
-jest.mock("../../src/worker/services/request");
+jest.mock("../../src/worker/services/appiumService");
 jest.mock("../../src/worker/stores/sessionStore");
 
 const { sessionStore } = require("../../src/worker/stores/sessionStore");
 const { appiumService } = require("../../src/worker/services/appiumService");
 const appdriverService = require("../../src/worker/services/appdriverService");
 const { setPlatform } = require("../helpers");
-const requestHelpers = require("../../src/worker/services/request");
 const gestures = require("../../src/worker/gestures");
 
 afterEach(() => {
@@ -14,7 +13,7 @@ afterEach(() => {
 });
 
 const runNativeTests = () => {
-  it("makes a POST request to the correct Appium endpoint", async () => {
+  it("calls the performActions method on the Appium service", async () => {
     const sessionId = "sessionId";
     const x = 100;
     const y = 200;
@@ -22,33 +21,25 @@ const runNativeTests = () => {
     const distance = 300;
     const actions = await gestures.scroll({ x, y, direction, distance }).resolve();
     jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
-    jest.spyOn(requestHelpers, "request").mockResolvedValue();
+    jest.spyOn(appiumService, "performActions").mockResolvedValue();
 
     await expect(appdriverService.scroll({ x, y, direction, distance }))
       .resolves.toEqual(undefined);
 
-    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-    expect(requestHelpers.request).toHaveBeenCalledWith({
-      method: "POST",
-      path: `/session/${sessionId}/actions`,
-      payload: { actions }
-    });
+    expect(appiumService.performActions).toHaveBeenCalledTimes(1);
+    expect(appiumService.performActions).toHaveBeenCalledWith({ sessionId, actions });
   });
 
   it("optionally accepts a sessionId", async () => {
     const sessionId = "newSessionId";
     jest.spyOn(sessionStore, "getSessionId").mockReturnValue("sessionId");
-    jest.spyOn(requestHelpers, "request").mockResolvedValue();
+    jest.spyOn(appiumService, "performActions").mockResolvedValue();
 
     await expect(appdriverService.scroll({ sessionId, x: 50, y: 200, direction: 0, distance: 100 }))
       .resolves.toEqual(undefined);
 
-    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-    expect(requestHelpers.request).toHaveBeenCalledWith({
-      method: "POST",
-      path: `/session/${sessionId}/actions`,
-      payload: expect.anything()
-    });
+    expect(appiumService.performActions).toHaveBeenCalledTimes(1);
+    expect(appiumService.performActions).toHaveBeenCalledWith(expect.objectContaining({ sessionId }));
   });
 };
 
@@ -65,39 +56,32 @@ describe("Android", () => {
 describe("Web", () => {
   beforeEach(() => setPlatform("Web"));
 
-  it("makes a POST request to the correct Appium endpoint", async () => {
+  it("calls the execute method on the Appium service", async () => {
     const sessionId = "sessionId";
     const distance = 300;
     jest.spyOn(sessionStore, "getSessionId").mockReturnValue(sessionId);
-    jest.spyOn(requestHelpers, "request").mockResolvedValue();
+    jest.spyOn(appiumService, "execute").mockResolvedValue();
 
     await expect(appdriverService.scroll({ x: 100, y: 200, direction: 180, distance }))
       .resolves.toEqual(undefined);
 
-    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-    expect(requestHelpers.request).toHaveBeenCalledWith({
-      method: "POST",
-      path: `/session/${sessionId}/execute`,
-      payload: {
-        script: "return window.scrollBy(arguments[0], arguments[1])",
-        args: [ 0, distance ]
-      }
+    expect(appiumService.execute).toHaveBeenCalledTimes(1);
+    expect(appiumService.execute).toHaveBeenCalledWith({
+      sessionId,
+      script: "window.scrollBy(arguments[0], arguments[1])",
+      args: [ 0, distance ]
     });
   });
 
   it("optionally accepts a sessionId", async () => {
     const sessionId = "newSessionId";
     jest.spyOn(sessionStore, "getSessionId").mockReturnValue("sessionId");
-    jest.spyOn(requestHelpers, "request").mockResolvedValue();
+    jest.spyOn(appiumService, "execute").mockResolvedValue();
 
     await expect(appdriverService.scroll({ sessionId, x: 50, y: 200, direction: 0, distance: 100 }))
       .resolves.toEqual(undefined);
 
-    expect(requestHelpers.request).toHaveBeenCalledTimes(1);
-    expect(requestHelpers.request).toHaveBeenCalledWith({
-      method: "POST",
-      path: `/session/${sessionId}/execute`,
-      payload: expect.anything()
-    });
+    expect(appiumService.execute).toHaveBeenCalledTimes(1);
+    expect(appiumService.execute).toHaveBeenCalledWith(expect.objectContaining({ sessionId }));
   });
 });
